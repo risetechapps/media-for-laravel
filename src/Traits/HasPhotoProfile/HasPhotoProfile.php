551d11bebb8660ab1007d664e5f46c15ac6d3d02
risetechapps <apps@risetech.com.br>
2024-12-19 14:13:32 -0300

    Criado trait para gerenciar photo profile.

---
+++ src/Traits/HasPhotoProfile/HasPhotoProfile.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace RiseTechApps\Media\Traits\HasPhotoProfile;
+
+use Illuminate\Database\Eloquent\Relations\HasOne;
+use RiseTechApps\Media\Models\Media;
+
+trait HasPhotoProfile
+{
+    public function getPhotoProfileUrl(): ?string
+    {
+        try {
+            $photo = $this->getMedia('profile')->first();
+
+            if (is_null($photo)) {
+                return null;
+            }
+
+            return $photo->getFullUrl();
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    public function getPhotoProfile(): ?Media
+    {
+        try {
+            $photo = $this->getMedia('profile')->first();
+
+            if (is_null($photo)) {
+                return null;
+            }
+
+            return $photo;
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+
+    public function profilePhoto(): HasOne
+    {
+        return $this->hasOne(Media::class, 'model_id')->where('collection_name', 'profile');
+    }
+}
