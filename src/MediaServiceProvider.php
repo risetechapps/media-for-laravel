b51af1658f2bc806da225fccdfea06f982507464
risetechapps <apps@risetech.com.br>
2025-02-12 09:01:22 -0300

    Atualizado parametros

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -55,7 +55,7 @@ class MediaServiceProvider extends ServiceProvider
     {
         $disks = $this->app['config']['filesystems.disks'];
 
-        $exclude = $this->app['config']['media.disk.exclude'];
+        $exclude = $this->app['config']['media.disk.exclude'] ?? [];
 
         $prefix = $this->app['config']['media.disk.prefix'];
 
