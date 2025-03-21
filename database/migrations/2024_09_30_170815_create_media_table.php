f7c3e036ec2280584677bc73998797755329f16a
risetechapps <apps@risetech.com.br>
2025-03-21 12:58:50 -0300

    Atualizado migrations.

--- database/migrations/2024_09_30_170815_create_media_table.php
+++ database/migrations/2024_09_30_170815_create_media_table.php
@@ -1,13 +1,21 @@
 <?php
 
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+use Tpetry\PostgresqlEnhanced\Schema\Blueprint;
+use Tpetry\PostgresqlEnhanced\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
     public function up(): void
     {
+
+        Schema::createExtensionIfNotExists('citext');
+
         Schema::create('media', function (Blueprint $table) {
             $table->id();
             $table->uuidMorphs('model');
@@ -19,13 +27,28 @@ return new class extends Migration
             $table->string('disk');
             $table->string('conversions_disk')->nullable();
             $table->unsignedBigInteger('size');
-            $table->json('manipulations');
-            $table->json('custom_properties');
-            $table->json('generated_conversions');
-            $table->json('responsive_images');
-            $table->unsignedInteger('order_column')->nullable()->index();
+            $table->jsonb('manipulations');
+            $table->jsonb('custom_properties');
+            $table->jsonb('generated_conversions');
+            $table->jsonb('responsive_images');
+            $table->unsignedInteger('order_column')->nullable();
             $table->softDeletes();
             $table->nullableTimestamps();
+
+            $table->index('collection_name');
+            $table->index('name');
+            $table->index('file_name');
+            $table->index('order_column');
         });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('media');
     }
 };
