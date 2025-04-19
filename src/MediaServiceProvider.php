73ca8fdb2186aa5594be6040a6247ddab60ff2a6
risetechapps <apps@risetech.com.br>
2025-04-19 00:20:25 -0300

    Registrado macro

--- src/MediaServiceProvider.php
+++ src/MediaServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace RiseTechApps\Media;
 
+use Illuminate\Routing\ResponseFactory;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\ServiceProvider;
@@ -34,6 +35,8 @@ class MediaServiceProvider extends ServiceProvider
         Media::observe(new MediaObserver);
 
         $this->setPrefixFilesystems();
+
+        $this->registerMacros();
     }
 
     /**
@@ -80,6 +83,43 @@ class MediaServiceProvider extends ServiceProvider
             $bar = empty($pathRoot) ? '' : '/';
 
             $this->app['config']["filesystems.disks.{$disk}.root"] = $pathRoot .  $bar  . "${prefix}";
+        }
+    }
+
+    protected function registerMacros(): void
+    {
+
+        if(!ResponseFactory::hasMacro('jsonSuccess')){
+            ResponseFactory::macro('jsonSuccess', function ($data = []) {
+                $response = ['success' => true];
+                if (!empty($data)) $response['data'] = $data;
+                return response()->json($response);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonError')){
+            ResponseFactory::macro('jsonError', function ($data = null) {
+                $response = ['success' => false];
+                if (!is_null($data)) $response['message'] = $data;
+                return response()->json($response, 412);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonGone')) {
+            ResponseFactory::macro('jsonGone', function ($data = null) {
+                $response = ['success' => false];
+                if (!is_null($data)) $response['message'] = $data;
+                return response()->json($response, 410);
+            });
+        }
+
+        if(!ResponseFactory::hasMacro('jsonNotValidated')) {
+            ResponseFactory::macro('jsonNotValidated', function ($message = null, $error = null) {
+                $response = ['success' => false];
+                if (!is_null($message)) $response['message'] = $message;
+
+                return response()->json($response, 422);
+            });
         }
     }
 }
