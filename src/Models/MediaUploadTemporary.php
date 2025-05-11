3065c56f13fc16b5c9a7e577b173f94cbe7397bc
risetechapps <apps@risetech.com.br>
2025-05-11 10:19:49 -0300

    Criado Prunable.

--- src/Models/MediaUploadTemporary.php
+++ src/Models/MediaUploadTemporary.php
@@ -3,6 +3,8 @@
 namespace RiseTechApps\Media\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Prunable;
+use Illuminate\Support\Facades\Storage;
 use RiseTechApps\HasUuid\Traits\HasUuid\HasUuid;
 use RiseTechApps\Media\Traits\HasConversionsMedia\HasConversionsMedia;
 use Spatie\MediaLibrary\HasMedia;
@@ -10,4 +12,25 @@ use Spatie\MediaLibrary\HasMedia;
 class MediaUploadTemporary extends Model implements HasMedia
 {
     use HasConversionsMedia, HasUuid;
+    use Prunable;
+
+    protected function pruning(): void
+    {
+        $media = $this->medias()->first();
+
+        $disk = $media->disk;
+        $path = $media->getPathRelativeToRoot();
+        $pathFolder = dirname($path);
+
+        if (Storage::disk($disk)->exists($pathFolder)) {
+            Storage::disk($disk)->deleteDirectory($pathFolder);
+        }
+
+        $media->forceDelete();
+    }
+
+    public function prunable(): \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Query\Builder
+    {
+        return static::where('created_at', '<=', now()->subDays(2));
+    }
 }
