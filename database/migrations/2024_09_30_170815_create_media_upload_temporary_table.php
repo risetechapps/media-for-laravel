4635a29fe0dae7c9671e800562619527a3394da6
risetechapps <apps@risetech.com.br>
2025-05-23 00:44:30 -0300

    Adicionadas verificações de driver para criação de colunas case-insensitive e extensão citext nas migrações

--- database/migrations/2024_09_30_170815_create_media_upload_temporary_table.php
+++ database/migrations/2024_09_30_170815_create_media_upload_temporary_table.php
@@ -4,13 +4,17 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     public function up(): void
     {
         Schema::create('media_upload_temporaries', function (Blueprint $table) {
             $table->uuid('id')->primary();
             $table->timestamps();
         });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('media_upload_temporaries');
     }
 };
