91c2ad9241afa4a36ef2e70d8a6bfd6c57b23c89
risetechapps <apps@risetech.com.br>
2026-04-29 12:33:50 -0300

    feat: adiciona trait unificada HasMediaSuite
      - Combina HasConversionsMedia e HasPhotoProfile em uma única trait
      - Facilita uso em models que precisam de todas as funcionalidades de mídia
      - Recomendado para User, Company, Employee e models similares

---
+++ src/Traits/HasMediaSuite/HasMediaSuite.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace RiseTechApps\Media\Traits\HasMediaSuite;
+
+use RiseTechApps\Media\Traits\HasConversionsMedia\HasConversionsMedia;
+use RiseTechApps\Media\Traits\HasPhotoProfile\HasPhotoProfile;
+
+trait HasMediaSuite
+{
+    use HasConversionsMedia, HasPhotoProfile;
+}
